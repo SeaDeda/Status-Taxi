af5c7edbd78597f4ed66f1fa23f4452b0329b240
SeaDeda <Laurahaq31@gmail.com>
2024-03-25 12:42:58 +0700

    Second Commit

---
+++ Taxi/Taxi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            layanan saya = new layanan();
+
+            saya.Supir = "Century";
+            saya.Status = true;
+            saya.Penumpang = 10;
+
+            saya.info();
+            saya.pickuppassenger();
+            saya.dropofpassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi/Taxi/layanan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class layanan
+    {
+        public string Supir {  get; set; }
+        public bool Status { get; set; }
+        public int Penumpang { get; set; }
+
+        public void info()
+        {
+            Console.WriteLine("Driver Name: {0}", Supir);
+            if (Status == true)
+            {
+                Console.WriteLine("On Duty: Yes");
+            }
+            Console.WriteLine("Number of Passenger: {0}\n", Penumpang);
+        }
+
+        public void pickuppassenger()
+        {
+            Console.WriteLine("{0} sedang dalam perjalanan", Supir);
+        }
+
+        public void dropofpassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", Supir);
+        }
+    }
+}
